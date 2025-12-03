fd4cfdda32ae5ee4370df0de9d976a10462210e2
luciec-dot <luciechinon@gmail.com>
2025-12-03 21:07:18 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,13 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+int somme(int a, int b);
+int difference(int a, int b);
+int produit(int a, int b);
+int quotient(int a, int b);
+int modulo(int a, int b);
+int et_bit(int a, int b);
+int ou_bit(int a, int b);
+int negation(int a, int b); // seul a est utilisé
+
+#endif
