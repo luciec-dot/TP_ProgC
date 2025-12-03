f557c9fae3c2bd4e49dbe717d6cb5c8718146ddd
luciec-dot <luciechinon@gmail.com>
2025-12-03 20:01:34 +0100

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    // 1. Types entiers
+    signed char c = -10;
+    unsigned char uc = 200;
+
+    signed short s = -32000;
+    unsigned short us = 65000;
+
+    signed int i = -100000;
+    unsigned int ui = 3000000000U;
+
+    signed long int li = -500000000;
+    unsigned long int uli
